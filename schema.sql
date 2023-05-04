2e34289e17634f31480c755c3528630f8eed8de5
ilyass atlassi <atlassiilyass@gmail.com>
2023-05-04 19:49:35 +0100

    Add new table

--- schema.sql
+++ schema.sql
@@ -11,6 +11,32 @@ CREATE TABLE animals (
   neutered BOOLEAN,
   weight_kg DECIMAL(5,2)
 );
-
+------------------------------------------------
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(50);
+---------------------------------------------
+
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  full_name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE species (
+  id SERIAL PRIMARY KEY,
+  name TEXT
+);
+
+ALTER TABLE animals
+DROP COLUMN species,
+ADD COLUMN species_id INT,
+ADD COLUMN owner_id INT,
+ADD CONSTRAINT fk_species
+  FOREIGN KEY (species_id)
+  REFERENCES species(id),
+ADD CONSTRAINT fk_owner
+  FOREIGN KEY (owner_id)
+  REFERENCES owners(id);
+
+
+
